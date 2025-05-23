bac1a0d93913cf940a148129c915a7da20a90da6
jesus portacio <jesusportacio33@gmail.com>
2025-05-23 18:43:42 -0500

    Código hecho en un sólo PC por Brito Esther, Cala Jhonny y Portacio Jesus

--- resources/views/calculadora.blade.php
+++ resources/views/calculadora.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de Notas del Semestre</title>
+    <title>Calculadora de Notas del Semestre universitarios</title>
     <link rel="stylesheet" href="{{ asset('styles.css') }}">
 </head>
 <body>
